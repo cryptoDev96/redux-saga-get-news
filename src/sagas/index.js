f7829ddf8f7a57193e816c9f6614d7b23dc9e8a6
cryptoDev96 <cryptoDev96@outlook.com>
2022-05-31 22:06:36 +0900

    second commit

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,7 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
 
+//When user click button, then action call actionWatcher in saga,
+//after this will call fetchNews function.
 function* fetchNews() {
   // const json = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc')
   const json = yield fetch('https://jsonplaceholder.typicode.com/posts')
